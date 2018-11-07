4d415b9628b1adeb5dd0d4a0d291ade5808bfc9f
Sohel Shaikh <rebht78@gmail.com>
2018-11-07 20:12:27 +0530

    added Program5 partial solution

---
+++ Program5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+/*
+Write a program to store information of student and display it using Stucture.
+
+Here, I have store 3 information of students : rollno, name and course.
+*/
+// declaration of structure
+struct Student
+{
+    int rollno;
+    char name[30];
+    char course[15];
+}
+void main()
+{
+
+    clrscr(); //clearing the screen
+
+    getch(); // printing the output on the screen
+}
