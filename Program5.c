52182724e0a86e4235ffe852f65f5734ca228c75
Sohel Shaikh <rebht78@gmail.com>
2018-11-08 11:35:28 +0530

    added complete solution of Program5

--- Program5.c
+++ Program5.c
@@ -11,11 +11,21 @@ struct Student
     int rollno;
     char name[30];
     char course[15];
-}
+};
 void main()
 {
-
+    // declaring variable of structure Student
+    struct Student std;
     clrscr(); //clearing the screen
+    printf ("Enter roll no: \n");
+    scanf("%d",&std.rollno);
+    printf ("Enter name: \n");
+    scanf("%s",&std.name);
+    printf ("Enter course: \n");
+    scanf("%s",&std.course);
 
+    printf("Student Roll No is %d\n",std.rollno);
+    printf("Student Name is %s\n",std.name);
+    printf("Student Course is %s\n",std.course);
     getch(); // printing the output on the screen
 }
